696a320f482005b1e00a7a13d52347a6d873417d
Sahil Saleem <70108175+sahilsaleem2907@users.noreply.github.com>
2021-02-22 23:27:48 +0530

    Update globalvariables.dart

--- cab_rider/lib/globalvariables.dart
+++ cab_rider/lib/globalvariables.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-String mapKey = 'AIzaSyCMY0WiYYW-LcNZBXaKhL5gqQldvf5p65Y';
+String mapKey = 'YOUR API KEY HERE';
 
 
 
